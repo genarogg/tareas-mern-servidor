221a7674155db2dc84108a4eb65241f9febf078e
Genarogg <genarrogg@gmail.com>
2020-09-05 21:00:16 -0400

    configurando la coneccion en el codigo

--- config/db.js
+++ config/db.js
@@ -1,1 +1,18 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+require("dotenv").config({ path: "variables.env" });
+
+const conectarDB = async () => {
+    try {
+        await mongoose.connect(process.env.DB_MONGO, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false
+        })
+    }
+    catch (error) {
+        console.log(error);
+        process.exit(1); // Detener la app
+    }
+}
+
+module.exports = conectarDB;

8deb3bd6fc01bc88b389e4498414d3ac44a00b4a
genaro gonzalez <genarrogg@gmail.com>
2022-10-06 22:54:06 -0400

    server

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors())
 app.use(express.json({ extended: true }));
 
 /* puerto de la app */
-const PORT = process.env.PORT || 4000;
+const port = process.env.port || 4000;
 
 /* importar rutas */
 app.use("/api/usuarios", require("./routes/usuarios"));
@@ -21,6 +21,6 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/proyectos", require("./routes/proyectos"));
 app.use("/api/tareas", require("./routes/tareas"));
 /* arrancar el app */
-app.listen(PORT, () => {
+app.listen(port,'0.0.0.0', () => {
   console.log(`El servidor esta funcionando en el port ${PORT}`);
 });

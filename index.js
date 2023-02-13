34bc1c190fe6d612c169a12a0781fcbfac38bc21
genaro gonzalez <genarrogg@gmail.com>
2023-02-12 21:06:17 -0400

    index

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ app.use(express.json({ extended: true }));
 /* puerto de la app */
 const PORT = process.env.PORT || 4000;
 
+app.get('/', (req, res) => {
+  res.send('La app esta funcionando')
+
+})
+
 /* importar rutas */
 app.use("/api/usuarios", require("./routes/usuarios"));
 app.use("/api/auth", require("./routes/auth"));
